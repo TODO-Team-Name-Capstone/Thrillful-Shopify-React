1d8efa3b0e197f452c36e2b4a87d6a61bd716ee7
Shawna Staff <staffs@my.lanecc.edu>
2021-06-19 15:03:29 -0700

    dynamically pulling data from the shopify dev store

--- frontend/src/components/NavMenu.js
+++ frontend/src/components/NavMenu.js
@@ -1,31 +1,48 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { ShopContext } from '../context/shopContext';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { ShopContext } from "../context/shopContext";
 import {
-    Drawer,
-    DrawerBody,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
-    VStack
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  VStack,
+  Center,
+  Box,
+  Text,
 } from "@chakra-ui/react";
 
-
-
 const NavMenu = () => {
+  const { isMenuOpen, closeMenu, collections } = useContext(ShopContext);
 
-    const { isMenuOpen, closeMenu} = useContext(ShopContext)
-
-    return (
-        <Drawer isOpen={isMenuOpen} onClose={closeMenu} placement="left" size="sm">
-            <DrawerOverlay>
-                <DrawerContent  bgGradient="linear(to-r, black 60%, #ff0000)">
-                    <DrawerCloseButton />
-                    <DrawerHeader textAlign="center" color="#ffffff" _hover={{ color:"#ff0000"}} ><Link to="/collections" style={{ fontWeight: "bold" }}onClick={closeMenu}>Categories</Link></DrawerHeader>
-                    <DrawerBody flexDirection="row" color="white" alignItems="center" justifyContent="space-between" >
-                        <VStack _hover={{ color:"#ff0000"}}  p="2rem">
+  return (
+    <Drawer isOpen={isMenuOpen} onClose={closeMenu} placement="left" size="sm">
+      <DrawerOverlay>
+        <DrawerContent bgGradient="linear(to-r, black 60%, #ff0000)">
+          <DrawerCloseButton />
+          <DrawerHeader
+            textAlign="center"
+            color="#ffffff"
+            _hover={{ color: "#ff0000" }}
+          >
+            <Link
+              to="/collections"
+              style={{ fontWeight: "bold" }}
+              onClick={closeMenu}
+            >
+              Categories
+            </Link>
+          </DrawerHeader>
+          <DrawerBody
+            flexDirection="row"
+            color="white"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            {/* <VStack _hover={{ color:"#ff0000"}}  p="2rem">
                             <Link to="/all-products" style={{ fontWeight: "bold" }}onClick={closeMenu}>All Products</Link>
                         </VStack>
                         <VStack _hover={{ color:"#ff0000"}} p="2rem">
@@ -39,17 +56,35 @@ const NavMenu = () => {
                         </VStack>
                         <VStack _hover={{ color:"#ff0000"}} p="2rem">
                             <Link to="/Z2lkOi8vc2hvcGlmeS9Db2xsZWN0aW9uLzI2NjQ5OTQ1NzE5Mg==" style={{ fontWeight: "bold" }}onClick={closeMenu}>Beauty</Link>
-                        </VStack>
-                    </DrawerBody>
-                    <VStack>
-                   <DrawerFooter color="white" _hover={{ color:"#ff0000"}}>
-                    <Link to="/about" textAlign="center" style={{ fontWeight: "bold" }}onClick={closeMenu}>About Us</Link>
-                    </DrawerFooter>
-                   </VStack>
-                </DrawerContent>
-            </DrawerOverlay> 
-        </Drawer>
-    )
-}
+                        </VStack> */}
+            {collections.map((collection) => (
+              <Link to={`/${collection.handle}`} key={collection.id}>
+                <VStack _hover={{ color: "#ff0000" }} p="2rem">
+                  <Center>
+                    <Text style={{ fontWeight: "bold" }}>
+                      {collection.title}
+                    </Text>
+                  </Center>
+                </VStack>
+              </Link>
+            ))}
+          </DrawerBody>
+          <VStack>
+            <DrawerFooter color="white" _hover={{ color: "#ff0000" }}>
+              <Link
+                to="/about"
+                textAlign="center"
+                style={{ fontWeight: "bold" }}
+                onClick={closeMenu}
+              >
+                About Us
+              </Link>
+            </DrawerFooter>
+          </VStack>
+        </DrawerContent>
+      </DrawerOverlay>
+    </Drawer>
+  );
+};
 
-export default NavMenu
+export default NavMenu;
